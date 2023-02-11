06905f8fdef712062cf0824a22ae9027998c3284
ravisumit33 <ravi.sumit33@gmail.com>
2023-02-11 22:04:43 +0530

    Simplify side module

--- side.cpp
+++ side.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 
-int my_create();
-
-struct my_struct {
-  int (*create)();
-};
-
-const my_struct mstrct = {my_create};
-
 int my_create() {
   printf("Inside my create\n");
   return 6;
 }
 
+auto fun_ptr = my_create;
+
 extern "C" {
 int fun() {
   printf("Inside fun\n");
-  const my_struct *ptr = &mstrct;
-  return ptr->create();
+  return fun_ptr();
 }
 }
